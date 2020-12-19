d0819ec2b31af174e776ea5415fd47388a026868
Rudresh Thakur <rudresh04thakur@gmail.com>
2020-12-19 12:21:00 +0530

    first commit

---
+++ inhertance.js
@@ -0,0 +1,42 @@
+function STUDENT(){
+    var name="";
+    var roll="";
+} //class
+
+STUDENT.prototype.getInput = function(){
+    STUDENT.prototype.name = prompt("Enter Name");
+    STUDENT.prototype.roll = parseInt(prompt("Enter roll numbmer"))
+}
+
+STUDENT.prototype.getOutput= function (){
+    document.write("<br>Name of student - " +  STUDENT.prototype.name);
+    document.write(`<br>Roll number ${STUDENT.prototype.roll}`);
+}
+
+
+
+function MARKS(){
+    var m1=0;
+    var m2=0;
+}
+
+MARKS.prototype = STUDENT.prototype // Inherit
+
+MARKS.prototype.getMarkInput = function(){
+    MARKS.prototype.m1 = parseInt(prompt("Enter M1 Mark "));
+    MARKS.prototype.m2 = parseInt(prompt("Enter M2 Mark"))
+}
+
+MARKS.prototype.getMarkOutput= function (){
+    document.write("<br>M1 - " +  MARKS.prototype.m1);
+    document.write(`<br>M2 ${MARKS.prototype.m2}`);
+}
+
+
+
+
+var m =new MARKS();
+m.getInput();
+m.getOutput()
+m.getMarkInput()
+m.getMarkOutput()
